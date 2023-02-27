ac7340f0db26e2e8f5b3ff23a3b7159984825216
Yonayv <fixicoyonayv@gmail.com>
2023-02-27 14:35:22 -0600

    doodoo

--- TypeCastingYonayv_Fixico/Program.cs
+++ TypeCastingYonayv_Fixico/Program.cs
@@ -6,7 +6,19 @@ namespace TypeCastingYonayv_Fixico
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Asks user to enter a whole number and assigns input to integer
+            Console.WriteLine("Please enter whole number!");
+            int myFavNum = Convert.ToInt32(Console.ReadLine());
+
+            // Prints favorite number to console
+            Console.WriteLine("Your favorite number is " + myFavNum);
+
+            // Asks user a true/false question and assigns response to a boolean
+            Console.WriteLine("Do you like video games? (please enter 'true' or 'false')");
+            bool isTrue = Convert.ToBoolean(Console.ReadLine());
+            
+            // Print the result of question asked
+            Console.WriteLine($"It is {isTrue} that I like video games!");
         }
     }
 }
